ee4c893f0c4c7a46c29b2fda8bda2c785db33e1b
gksxodnd007 <gksxodnd007@gmail.com>
2021-02-14 19:56:52 +0900

    init commit

---
+++ consumer/src/main/java/org.codingsquid.consumer/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package org.codingsquid.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author taewoong.han
+ * @since 2021.02.12
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(ConsumerApplication.class);
+    }
+}
---
+++ producer/src/main/java/org/codingsquid/producer/ProducerApplication.java
@@ -0,0 +1,16 @@
+package org.codingsquid.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author taewoong.han
+ * @since 2021.02.12
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(ProducerApplication.class);
+    }
+}
