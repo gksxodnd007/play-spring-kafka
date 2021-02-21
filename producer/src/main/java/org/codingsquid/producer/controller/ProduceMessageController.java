adc23da271851fc9b4c15948c0734b267ec70575
gksxodnd007 <gksxodnd007@gmail.com>
2021-02-22 01:30:41 +0900

    Remove unused import

--- producer/src/main/java/org/codingsquid/producer/controller/ProduceMessageController.java
+++ producer/src/main/java/org/codingsquid/producer/controller/ProduceMessageController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
